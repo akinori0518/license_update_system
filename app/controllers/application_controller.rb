1f7338784df74b769ecda63aa74c7c66a8260a38
akinori0518 <aki.yamamoto0518@gmail.com>
2020-10-10 14:45:41 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインに成功しました" 
+    informations_new_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path 
+  end
+
   def configure_permitted_parameters
     #strong parametersを設定し、usernameを許可
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :password, :password_confirmation) }
